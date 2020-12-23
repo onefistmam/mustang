a4fe67f198921f3325abce9fade6675c7015de17
cuijiangkun <cui.jiangkun@upex.co>
2020-12-23 20:04:53 +0800

    init

--- befh/exchange/websocket_exchange.py
+++ befh/exchange/websocket_exchange.py
@@ -42,18 +42,19 @@ class WebsocketExchange(RestApiExchange):
         LOGGER.info("exchange load %s", self._name)
         contract_exchanges_use_common_channel = ['HuobiSwap', 'HuibiDM', 'KrakenFutures', 'BinanceFutures', 'Bitmex']
         if self._is_orders:
-            if self._type == 'spot' or self._name in contract_exchanges_use_common_channel:
-                channels = [TRADES, L2_BOOK, BOOK_TICKER]
-            elif self._type == 'futures':
-                channels = [TRADES, L2_BOOK, BOOK_TICKER]
+            # if self._type == 'spot' or self._name in contract_exchanges_use_common_channel:
+            #     channels = [TRADES, L2_BOOK, BOOK_TICKER]
+            # el
+            if self._type == 'futures':
+                channels = [L2_BOOK, BOOK_TICKER]
             elif self._type == 'swap':
-                channels = [TRADES, L2_BOOK, BOOK_TICKER]
+                channels = [L2_BOOK, BOOK_TICKER]
 
             LOGGER.info("channel %s ", channels)
             callbacks = {
-                channels[0]: TradeCallback(self._update_trade_callback),
+                # channels[0]: TradeCallback(self._update_trade_callback),
                 L2_BOOK: BookCallback(self._update_order_book_callback),
-                TICKER: TickerCallback(self._update_ticker_callback),
+                # TICKER: TickerCallback(self._update_ticker_callback),
                 BOOK_TICKER: BookTickerCallback(self._update_book_ticker_callback)
             }
         else:
@@ -63,7 +64,6 @@ class WebsocketExchange(RestApiExchange):
                 channels = [TRADES]
             elif self._type == 'swap':
                 channels = [TRADES]
-
             callbacks = {
                 channels[0]: TradeCallback(self._update_trade_callback),
             }
@@ -120,7 +120,8 @@ class WebsocketExchange(RestApiExchange):
     def _update_order_book_callback(self, feed, pair, book, timestamp, receipt_timestamp):
         """Update order book callback.
         """
-        # LOGGER.info("order_book_callback,pair=%s , boo=%s", pair, str(book))
+
+        LOGGER.info("order_book_callback,pair=%s, feed=%s, boo=%s", pair, feed, book)
         if pair in self._instrument_mapping:
             # The instrument pair can be mapped directly from crypofeed
             # format to the ccxt format
@@ -151,6 +152,7 @@ class WebsocketExchange(RestApiExchange):
         self, feed, pair, order_id, timestamp, side, amount, price, receipt_timestamp):
         """Update trade callback.
         """
+        # LOGGER.info("_update_trade_callback,amount =%s", amount)
         instmt_info = self._instruments[self._instrument_mapping[pair]]
         trade = {}
 
@@ -168,22 +170,19 @@ class WebsocketExchange(RestApiExchange):
         trade['id'] = order_id
         trade['price'] = float(price)
         trade['amount'] = float(amount)
-
         current_timestamp = datetime.utcnow()
-
         if not instmt_info.update_trade(trade, current_timestamp):
             return
-
         for handler in self._handlers.values():
             instmt_info.update_table(handler=handler)
-
         self._rotate_ordre_tables()
 
     # (feed, pair, bid, ask, timestamp, receipt_timestamp)
     def _update_ticker_callback(self, feed, pair, last_price, first_bid, first_ask, timestamp, receipt_timestamp):
-        LOGGER.info("_update_ticker_callback, feed=%s, pair=%s, last_price=%s, first_bid=%s, first_ask=%s", feed, pair,
-                    last_price,
-                    first_bid, first_ask)
+        # LOGGER.info("_update_ticker_callback, feed=%s, pair=%s, last_price=%s, first_bid=%s, first_ask=%s", feed, pair,
+        #             last_price,
+        #             first_bid, first_ask)
+        return
 
     # (feed, pair, bid, ask, timestamp, receipt_timestamp)
     def _update_book_ticker_callback(self, feed, pair, last_price, first_bid, first_ask, timestamp,
